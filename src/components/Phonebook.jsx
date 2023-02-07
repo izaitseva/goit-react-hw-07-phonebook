d6d8b40b36fc2d70f0cc3f774edeacfacd0f0cfd
Iryna Zaitseva <zaitseva02@gmail.com>
2023-02-07 19:39:18 +0200

    number input

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -5,24 +5,32 @@ export class Phonebook extends React.Component {
 
     state = {
         contacts: [],
-        name: ''
+        name: '',
+        number: ''
     }
 
     nameInputId = nanoid();
 
-    handleChange = e => {
+    handleChangeName = e => {
         const { value } = e.currentTarget;
 
         this.setState({ name: value });
     }
 
+    handleChangeNumb = e => {
+        const { value } = e.currentTarget;
+
+        this.setState({ number: value });
+    }
+
     handleSubmit = event => {
         event.preventDefault();
         const contacts = this.state.contacts;
-        
+
         contacts.push({
             id: nanoid(),
-            name: this.state.name
+            name: this.state.name,
+            number: this.state.number
         });
 
         this.setState({
@@ -44,18 +52,27 @@ export class Phonebook extends React.Component {
                         type="text"
                         name="name"
                         value={this.state.value}
-                        onChange={this.handleChange}
+                        onChange={this.handleChangeName}
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                         id={this.nameInputId}
+                    />
+                    <input
+                        type="tel"
+                        name="number"
+                        value={this.state.value}
+                        onChange={this.handleChangeNumb}
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
                     />
                     <button type="submit">Add contact</button>
                 </form>
                 <h2>Contacts</h2>
                 <ul>
                     {
-                        this.state.contacts.map(el => (<li key={el.id}>{el.name}</li>))
+                        this.state.contacts.map(el => (<li key={el.id}>{el.name}:{el.number}</li>))
                     }
                 </ul>
             </div>
