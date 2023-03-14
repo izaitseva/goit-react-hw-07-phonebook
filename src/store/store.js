697dc315da1b7582fd96d6fa7efca9f9fe68a5e0
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-14 16:25:35 +0200

    persist error

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+    persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts.slice';
 
@@ -16,6 +18,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 const persistor = persistStore(store);
